a0697d8504f88f5b3ce92fe1eb8afa35ed6d15b9
Jan Harrucksteiner <113837032+avexyy@users.noreply.github.com>
2023-10-12 20:39:18 +0200

    Add files via upload

---
+++ BankLogic.cs
@@ -0,0 +1,47 @@
+﻿namespace Bank.Logic;
+
+public abstract class Account
+{
+    public string AccountNumber { get; set; } = "";
+    public string AccountHolder { get; set; } = "";
+    public decimal CurrentBalance { get; set; }
+    public abstract bool IsAllowed(Transaction transaction);
+
+    public bool TryExecute(Transaction transaction)
+    {
+        if (IsAllowed(transaction))
+        {
+            CurrentBalance += transaction.Amount;
+            return true;
+        }
+        return false;
+    }
+}
+public class Money : Account
+{
+    public override bool IsAllowed(Transaction transaction)
+    {
+        return transaction.AccountNumber == AccountNumber && (CurrentBalance + transaction.Amount) is >= 0 and <= 100000000;
+    } 
+}
+public class CheckingAccount : Account
+{
+    public override bool IsAllowed(Transaction transaction)
+    {
+        return transaction.AccountNumber == AccountNumber && (CurrentBalance + transaction.Amount) is >= -10000 and <= 10000000 && Math.Abs(transaction.Amount) <= 10000 ; 
+    }
+}
+public class BusinessAccount : Account
+{
+    public override bool IsAllowed(Transaction transaction)
+    {
+        return transaction.AccountNumber == AccountNumber && (CurrentBalance + transaction.Amount) is >= -1000000 and <= 100000000 && Math.Abs(transaction.Amount) <= 100000 ;
+    }
+}
+public class Transaction
+{
+    public string AccountNumber { get; set; } = "";
+    public string Description { get; set; } = "";
+    public decimal Amount { get; set; }
+    public DateTime Timestamp { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using Bank.Logic;
+
+Account account;
+
+Console.Write("Type of account ([c]hecking, [b]usiness, [s]avings): ");
+
+switch (Console.ReadLine()!)
+{
+    case "c": account = new CheckingAccount(); break;
+    case "b": account = new BusinessAccount(); break;
+    case "s": account = new Money(); break;
+    default: Console.WriteLine("Invalid input"); return;
+}
+
+Console.Write("Account number: ");
+account.AccountNumber = Console.ReadLine()!;
+
+Console.Write("Account holder: ");
+account.AccountHolder = Console.ReadLine()!;
+
+Console.Write("Current balance: ");
+account.CurrentBalance = decimal.Parse(Console.ReadLine()!);
+
+var transaction = new Transaction();
+
+Console.Write("Transaction acount number: ");
+transaction.AccountNumber = Console.ReadLine()!;
+
+Console.Write("Transaction description: ");
+transaction.Description = Console.ReadLine()!;
+
+Console.Write("Transaction amount: ");
+transaction.Amount = decimal.Parse(Console.ReadLine()!);
+
+Console.Write("Transaction timestamp: ");
+transaction.Timestamp = DateTime.Parse(Console.ReadLine()!);
+
+if (account.TryExecute(transaction))
+{
+    Console.WriteLine($"Transaction executed successfully. The new current balance is {account.CurrentBalance}.");
+}
+else
+{
+    Console.WriteLine("Transaction not allowed.");
+}
